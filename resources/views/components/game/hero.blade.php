aef5024e18c59637529f8e115c29e4207a893df6
Anton Kazakov <ant.kazakov99@gmail.com>
2026-07-27 22:23:11 +0300

    feat: add game detail page markup

--- resources/views/components/game/hero.blade.php
+++ resources/views/components/game/hero.blade.php
@@ -1,4 +1,8 @@
-<h1>{{ $game['name'] }}</h1>
-@isset($game['cover_path'])
-    <img src="{{ $game['cover_path'] }}" alt="{{ $game['name'] }}"/>
-@endisset
+<section class="grid grid-cols-[max-content_auto] gap-5">
+    <div class="w-2xs aspect-3/4 rounded-2xl overflow-hidden bg-linear-to-bl from-gray-600 to-gray-700">
+        @isset($game['cover_path'])
+            <img src="{{ $game['cover_path'] }}" alt="{{ $game['name'] }}" class="size-full object-cover"/>
+        @endisset
+    </div>
+    <h1 class="text-3xl font-bold text-gray-900">{{ $game['name'] }}</h1>
+</section>
