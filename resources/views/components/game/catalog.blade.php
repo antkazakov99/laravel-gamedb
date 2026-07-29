70197f0599c4c90ae5bee68d9ef4c67e9c3aa533
Anton Kazakov <ant.kazakov99@gmail.com>
2026-07-29 13:59:51 +0300

    style: add game catalog page markup

--- resources/views/components/game/catalog.blade.php
+++ resources/views/components/game/catalog.blade.php
@@ -1,5 +1,18 @@
-@forelse($games as $game)
-    <div><a href="/game/{{ $game['id'] }}/{{ $game['slug'] }}">{{ $game['name'] }}</a></div>
-@empty
-    Nothing to see here
-@endforelse
+<section>
+    <div class="grid grid-cols-2 gap-5">
+        @forelse($games as $game)
+            <div class="grid grid-cols-[max-content_auto] gap-5 p-2 rounded-2xl shadow-2xl">
+                <div class="w-32 aspect-3/4 rounded-2xl overflow-hidden bg-linear-to-bl from-gray-600 to-gray-700">
+                    @isset($game['cover_path'])
+                        <img src="{{ $game['cover_path'] }}" alt="{{ $game['name'] }}" class="size-full object-cover"/>
+                    @endisset
+                </div>
+                <div>
+                    <a href="/game/{{ $game['id'] }}/{{ $game['slug'] }}" class="font-bold text-gray-900">{{ $game['name'] }}</a>
+                </div>
+            </div>
+        @empty
+            Nothing to see here
+        @endforelse
+    </div>
+</section>
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -3,6 +3,5 @@
         Catalog
     </x-slot:title>
 
-    <h1>Catalog</h1>
     <x-game.catalog :$games />
 </x-public>
